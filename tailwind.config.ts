7311714d496b60a6d0f12f4f17eaeaccc2504350
Ablyazov Dariy <mr96crusher@gmail.com>
2025-01-15 12:58:30 +0100

    add and configure tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import colors from 'tailwindcss/colors';
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: colors.blue,
+        secondary: colors.slate,
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+} satisfies Config;
