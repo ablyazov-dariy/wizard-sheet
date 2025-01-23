28fc7ee28a349814d0f001490e8c998df5d58e00
Ablyazov Dariy <mr96crusher@gmail.com>
2025-01-23 21:06:57 +0100

    style menu

--- src/app/components/game-table/game-table-head/game-table-head.component.ts
+++ src/app/components/game-table/game-table-head/game-table-head.component.ts
@@ -1,11 +1,15 @@
 import { Component, inject, input } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { NgOptimizedImage } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+
 import { GameService } from '@services/game/game.service';
 import { EndGameConfirmDialogComponent } from '@components/end-game-confirm-dialog/end-game-confirm-dialog.component';
+
 import { take } from 'rxjs';
 
 @Component({
@@ -15,6 +19,7 @@ import { take } from 'rxjs';
     NgOptimizedImage,
     MatMenuModule,
     MatButtonModule,
+    MatIconModule,
   ],
   templateUrl: './game-table-head.component.html',
   styleUrl: './game-table-head.component.scss',
