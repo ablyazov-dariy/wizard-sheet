c33a258e7c11d957760a172de739c4fbf266a897
Ablyazov Dariy <mr96crusher@gmail.com>
2025-01-28 19:11:25 +0100

    fox focus

--- src/app/components/dialogs/game-config-form-dialog/game-config-form-dialog.component.ts
+++ src/app/components/dialogs/game-config-form-dialog/game-config-form-dialog.component.ts
@@ -2,11 +2,10 @@ import { Component, inject } from '@angular/core';
 import { GameConfigService } from '@services/game-config/game-config.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatDialogClose } from '@angular/material/dialog';
-import { PreventInvalidInputDirective } from '@app/directives/prevent-invalid-input/prevent-invalid-input.directive';
 
 @Component({
   selector: 'app-game-config-form-dialog',
-  imports: [ReactiveFormsModule, MatDialogClose, PreventInvalidInputDirective],
+  imports: [ReactiveFormsModule, MatDialogClose],
   templateUrl: './game-config-form-dialog.component.html',
   styleUrl: './game-config-form-dialog.component.scss',
 })
--- src/app/components/game-table/game-table-body/game-table-body.component.ts
+++ src/app/components/game-table/game-table-body/game-table-body.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import {
   FormArray,
   FormGroup,
@@ -10,6 +10,7 @@ import { FormControls } from '@interfaces/util/form-controls.type';
 import { Trick } from '@interfaces/trick.interface';
 import { NgClass } from '@angular/common';
 import { PreventInvalidInputDirective } from '@app/directives/prevent-invalid-input/prevent-invalid-input.directive';
+import { PlayersService } from '@services/players/players.service';
 
 @Component({
   selector: 'app-game-table-body',
@@ -26,19 +27,37 @@ import { PreventInvalidInputDirective } from '@app/directives/prevent-invalid-in
 export class GameTableBodyComponent {
   roundsControls = input<FormArray<FormGroup<FormControls<Trick>>>[]>();
 
-  tricksBidTabIndex(row: number): number {
-    if (row % 2 !== 0) {
-      return row * 2 + 2;
-    } else {
-      return row * 2 + 1;
-    }
+  private playersService = inject(PlayersService);
+
+  getTabIndex(row: number, column: number, isTricksWon: boolean): number {
+    const players = this.playersService.form.length;
+
+    const offset = isTricksWon ? column + 1 + players : column + 1;
+
+    return (
+      row * (players * 2) +
+      offset +
+      this.rowModOffset(row + 1, column + 1, players)
+    );
   }
 
-  tricksWonTabIndex(row: number): number {
-    if (row % 2 !== 0) {
-      return row * 2 + 3;
+  private rowModOffset(r: number, c: number, players: number): number {
+    const rowMod = r % players;
+
+    if (rowMod === 1) {
+      return 0;
+    } else if (rowMod === 0) {
+      if (c === players) {
+        return 1 - players;
+      } else {
+        return 1;
+      }
     } else {
-      return row * 2 + 2;
+      if (c < rowMod) {
+        return 1 + players - rowMod;
+      } else {
+        return 1 - rowMod;
+      }
     }
   }
 }
